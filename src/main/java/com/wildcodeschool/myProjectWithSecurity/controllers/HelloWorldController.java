b4cb29dbbc9a56c35634aa98aa93e7e733ca6e38
Mbirepinte <mickael.birepinte@gmail.com>
2023-03-13 12:35:02 +0100

    quest done

---
+++ src/main/java/com/wildcodeschool/myProjectWithSecurity/controllers/HelloWorldController.java
@@ -0,0 +1,58 @@
+package com.wildcodeschool.myProjectWithSecurity.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    public static List<String> SECRET_BASES = Arrays.asList(
+        "Biarritz",
+        "Bordeaux",
+        "La Loupe",
+        "Lille",
+        "Lyon",
+        "Nantes",
+        "Orléans",
+        "Paris",
+        "Reims",
+        "Strasbourg",
+        "Toulouse",
+        "Amsterdam",
+        "Barcelone",
+        "Berlin",
+        "Lisbonne",
+        "Madrid" );
+
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to the SHIELD!";
+    }
+
+    @GetMapping("/secret-bases")
+    public List<String> admin() {
+        return SECRET_BASES;
+    }
+
+    @GetMapping("/avengers/assemble")
+    public String user() {
+        return "Avengers... Assemble";
+    }
+
+    @GetMapping("/access-denied")
+    public String accesDenied() {
+        return "GO AWAY!!!";
+    }
+
+    @GetMapping("/404")
+    public String accesNotFound() {
+        return "Are you lost ? We have not found your request. This is a 404 page.";
+    }
+
+
+}
